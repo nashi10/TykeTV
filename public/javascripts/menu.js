1c6a249d07b3c4c4681c21ab7a1aee92bedeaf58
nashi10 <nanditha.soundararaj@gmail.com>
2017-04-25 02:49:58 -0500

    CSS changes

--- public/javascripts/menu.js
+++
@@ -1,8 +0,0 @@
-
-//on click of link get delete account page by passing location storage email
-//'/delete.htm/:loginEmail'
-$("button").click(function(){
-    $.get("demo_test.asp", function(data, status){
-        alert("Data: " + data + "\nStatus: " + status);
-    });
-});
